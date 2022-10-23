25337bda3b5ec11a4e7a0c75f6c7d2842794116e
Mücahit Eren Cantemür <eren.cantemur@jekirdek.com>
2022-10-23 18:32:58 +0300

    Created Contact Page

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Script from 'next/script'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script src="\node_modules\flowbite\dist\flowbite.js" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+
+
 }
 
 export default MyApp
---
+++ app/pages/contact.js
@@ -0,0 +1,12 @@
+import ContactForm from "../src/components/ContactForm";
+import Layout from "../src/containers/Layout";
+
+function Contact() {
+    return (
+        <Layout>
+            <ContactForm />
+        </Layout>
+    );
+}
+
+export default Contact;
